e9f40d437b379f5bf01c59cb3a313fee10990df2
hugokishi <hugopacheco370@gmail.com>
2022-02-03 00:32:01 -0300

    Added help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,9 @@
 module.exports = (client, msg) => {
-  msg.reply("Help Command");
+  msg.reply(`Você pode utilizar estes comandos: :smile:
+
+    s!play [URL] -> Comando para reproduzir música.
+    s!stop -> Comando para parar a música atual.
+    s!skip -> Comando para pular a música que está tocando agora.
+    s!queue -> Comando para ver a fila de espera de música.
+  `);
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
 
-const prefix = "s!";
+const config = require("../config.json");
 
-const commands = require("./scripts/command")(prefix);
+const commands = require("./scripts/command")(config.prefix);
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
 });
 
 client.on("ready", () => {
   console.log(`Sapphire Bot is ready to go! ${client.user.tag}`);
+  client.user.setActivity("Arctic Monkeys", { type: "LISTENING" });
+});
+
+client.on("messageCreate", async (message) => {
+  if (
+    message.author.bot ||
+    message.channel.type === "dm" ||
+    !message.content.startsWith(config.prefix)
+  )
+    return;
+
+  const args = message.content.split(" ");
+
+  if (commands[args[0]]) commands[args[0]](client, message);
 });
 
 client.login(process.env.BOT_TOKEN);
